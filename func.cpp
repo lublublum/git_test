f9c8c64499320601d6fd6a42d93a7eac82572217
lublublum <potrebichmd@gmail.com>
2023-10-17 13:35:14 +0300

    created func.cpp and moving func code from func.h to func.cpp

---
+++ func.cpp
@@ -0,0 +1,8 @@
+#include "func.h"
+
+int fact(int factValue)
+{
+    if (factValue <= 0) return  0;
+    if (factValue == 1) return  1;
+    return factValue * fact(factValue - 1);
+}
--- func.h
+++ func.h
@@ -1,8 +1,4 @@
 #pragma once
-int fact(int factValue)
-{
-    if (factValue <= 0) return  0;
-    if (factValue == 1) return  1;
-    return factValue * fact(factValue - 1);
-}
+int fact(int factValue);
+
 
